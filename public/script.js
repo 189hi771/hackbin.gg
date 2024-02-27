9ddd7d9f9768899ceda2388e008bcedbcd7843af
189hi <76269179+189hi771@users.noreply.github.com>
2024-02-27 16:28:22 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,38 +1,41 @@
 document.addEventListener("DOMContentLoaded", function () {
-    function generateRandomFileName() {
-        const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-        let result = "";
-        for (let i = 0; i < 8; i++) {
-            result += characters.charAt(Math.floor(Math.random() * characters.length));
-        }
-        return result;
-    }
+	function generateRandomFileName() {
+		const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		let result = "";
+		for (let i = 0; i < 8; i++) {
+			result += characters.charAt(Math.floor(Math.random() * characters.length));
+		}
+		return result;
+	}
 
-    document.getElementById("saveButton").addEventListener("click", function () {
-        const codeInput = document.getElementById("codeInput");
-        const codeContent = codeInput.value;
+	document.getElementById("saveButton").addEventListener("click", function () {
+		const codeInput = document.getElementById("codeInput");
+		const codeContent = codeInput.value;
 
-        if (codeContent.trim() !== "") {
-            const fileName = generateRandomFileName();
-            fetch('/scripts', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ fileName, codeContent }),
-            })
-            .then(response => response.text())
-            .then(extractedFileName => {
-                alert(`File created successfully. Filename: ${extractedFileName}`);
-                window.location.href = `/scripts/${extractedFileName}`;
-            })
-            .catch(error => {
-                console.error('Error:', error);
-                alert('Error saving file.');
-            });
+		if (codeContent.trim() !== "") {
+			const fileName = generateRandomFileName();
 
-        } else {
-            alert("Please enter code before saving.");
-        }
-    });
+			fetch('/scripts', {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'text/plain',
+				},
+				body: codeContent,
+			})
+			.then(response => response.text())
+			.then(message => {
+				// Extract the filename from the message
+				const extractedFileName = message.match(/Filename: (.+)/)[1];
+				alert(`File created successfully. Filename: ${extractedFileName}`);
+				// Redirect to the newly created page without the ".html" extension
+				window.location.href = `/scripts/${extractedFileName}`;
+			})
+			.catch(error => {
+				console.error('Error:', error);
+				alert('Error saving file.');
+			});
+		} else {
+			alert("Please enter code before saving.");
+		}
+	});
 });
