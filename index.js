674dcb3318107eb4a14e0c2442c60a4a58bb5d98
189hi <76269179+189hi771@users.noreply.github.com>
2024-02-27 16:27:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,31 +4,41 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = 3000;
 
 app.use(express.static('public'));
-app.use(bodyParser.json());
+app.use(bodyParser.text());
 
 app.post('/scripts', (req, res) => {
-    const { fileName, codeContent } = req.body;
+	const codeContent = req.body;
 
-    if (codeContent.trim() !== "") {
-        const filePath = path.join(__dirname, 'public', 'scripts', `${fileName}.html`);
+	if (codeContent.trim() !== "") {
+		const fileName = generateRandomFileName();
+		const filePath = path.join(__dirname, 'public', 'scripts', `${fileName}.html`);
 
-        fs.writeFile(filePath, codeContent, (err) => {
-            if (err) {
-                console.error(err);
-                res.status(500).send('Internal Server Error');
-            } else {
-                console.log(`File ${fileName}.html created successfully.`);
-                res.send(fileName);
-            }
-        });
-    } else {
-        res.status(400).send('Bad Request: Please provide code content.');
-    }
+		fs.writeFile(filePath, codeContent, (err) => {
+			if (err) {
+				console.error(err);
+				res.status(500).send('Internal Server Error');
+			} else {
+				console.log(`File ${fileName}.html created successfully.`);
+			return res.send(`File created successfully. Filename: ${fileName}.html`);
+			}
+		});
+	} else {
+		res.status(400).send('Bad Request: Please provide code content.');
+	}
 });
 
+function generateRandomFileName() {
+	const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	let result = "";
+	for (let i = 0; i < 8; i++) {
+		result += characters.charAt(Math.floor(Math.random() * characters.length));
+	}
+	return result;
+}
+
 app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+	console.log(`Server is running at http://localhost:${PORT}`);
 });
