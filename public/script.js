ac84523b2e41a9062d2b894100552802636e1163
189hi <76269179+189hi771@users.noreply.github.com>
2024-02-27 16:01:47 +0200

    Update script.js

--- public/script.js
+++ public/script.js
@@ -14,24 +14,23 @@ document.addEventListener("DOMContentLoaded", function () {
 
         if (codeContent.trim() !== "") {
             const fileName = generateRandomFileName();
-fetch('/public/scripts', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ fileName, codeContent }),
-})
-
+            fetch('/scripts', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ fileName, codeContent }),
+            })
             .then(response => response.text())
             .then(extractedFileName => {
                 alert(`File created successfully. Filename: ${extractedFileName}`);
-                // Redirect to the newly created page without the ".html" extension
                 window.location.href = `/scripts/${extractedFileName}`;
             })
             .catch(error => {
                 console.error('Error:', error);
                 alert('Error saving file.');
             });
+
         } else {
             alert("Please enter code before saving.");
         }
